9ed9d7cb31fb1d3c3abe5b42072bfadc27e905f9
Dongdong Kong <kongdd@users.noreply.github.com>
2026-08-21 16:58:58 +0800

    Expose optional river vector reader

--- src/RiverGraphs.jl
+++ src/RiverGraphs.jl
@@ -24,12 +24,23 @@ include("utils.jl")
 include("sf.jl")
 
 
+"""
+    read_river_lines(source; kwargs...)
+
+Read river centreline geometries as vectors of `(x, y)` coordinate tuples.
+Methods for GIS vector files are provided by the optional ArchGDAL extension;
+load ArchGDAL before calling this function on a file path.
+"""
+function read_river_lines end
+
+
 export RiverGraph
 export active_indices, reverse_index, pcr_dir,
   graph_flow,
   topological_sort_kahn,
   stream_order, stream_link, stream_network,
   fillnodata_upbasin, fillnodata_upriver
+export read_river_lines
 
 const path_flowdir_GuanShan = abspath("$(@__DIR__)/../data/GuanShan_flwdir.tif")
 
