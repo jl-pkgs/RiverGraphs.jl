01f345201c482a6b8e2d0df436f43db3adc41bbb
Dongdong Kong <kongdd@users.noreply.github.com>
2026-08-21 16:49:33 +0800

    Include priority flood routing

--- src/RiverGraphs.jl
+++ src/RiverGraphs.jl
@@ -9,6 +9,7 @@ import RTableTools: cbind, fwrite, fread
 include("IO.jl")
 include("RiverGraph.jl")
 include("constrained_flowdir.jl")
+include("priority_flood.jl")
 include("fillnodata_upbasin.jl")
 include("fillnodata_upriver.jl")
 
